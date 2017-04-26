702d646ec666aa203ddc35dda7f17fdb6218ec21
Zachary Durland <zacharydurland@gmail.com>
2017-04-27 08:58:14 +0900

    clear up and spaced out with better console logs

--- js/index.js
+++ js/index.js
@@ -9,9 +9,10 @@
   };
   var allPossNpcMoves = [];
   var possNpcMoves = [];
-  var moveCount = 0;
-  // these are possible winning lines
-  var boardLines = {
+  var thisLinesOpenings = 0;
+  
+
+  var boardLines = { // these are possible winning lines
     1 : ["1","2","3"], // 1,2,3
     2 : ["1","4","7"], // 1,4,7
     3 : ["1","5","9"], // 1,5,9
@@ -28,34 +29,38 @@
     var spaceIdRaw = this.id; // string "1"
     var spaceId = spaceIdRaw.slice(1);
     console.log(spaceId);
+    
     if($(this).html() === "") { // if space empty
-      console.log("empty"); 
+      console.log("Where player clicked was empty"); 
       $(this).html(playerMark); // put player's mark
-      moveCount += 1;
-      console.log("move count: " + moveCount);
       for( var i = 1; i < keys.length; i++) { // cycle through keys
         for ( var j = 0; j < 3; j++ ) { // cycle through values
-          if ( spaceId === boardLines[i][j] ) { 
+            thisLinesOpenings = 0;
+            if(boardLines[i][j] === ""){ // if just 1 open
+                thisLinesOpenings++;
+            }
+            
+            if ( spaceId === boardLines[i][j] ) { // get winning line 
             console.log("this space was found in one of the lines");
             allPossNpcMoves.push(...boardLines[i]);
             possNpcMoves = allPossNpcMoves.filter(function(possibleMoves){
               return possibleMoves !== spaceId;
             });
-            console.log(possNpcMoves);
+            console.log("NPC poss moves are " + possNpcMoves);
+                
           } // end check for space in a winning row
         } // end inner loop that searches a keys values to match the click
       } // end outer loop to loop through keys
     } // end check if space blank
-    for( var i = 0 ; i < possNpcMoves.length ; i++ ){ // loop through poss moves
+    
+      for( var i = 0 ; i < possNpcMoves.length ; i++ ){ // loop through poss moves
       if($("#s" + possNpcMoves[i]).html() === ""){ // if space is empty
         $("#s" + possNpcMoves[i]).html(npcMark); // mark npcs mark
-        moveCount += 1;
-        console.log("move count: " + moveCount);
         console.log("tried to mark npc in slot " + possNpcMoves[i]);
         break; // was successfull, so loop can stop
       }
     }
-    if (moveCount === 9) {
-      console.log("game over");
-    }
+      // put AI here
   });
+
+
