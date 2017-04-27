47daafd5f2c22f2c89f03654ea347eb98de9ff75
Zachary Durland <zacharydurland@gmail.com>
2017-04-27 09:36:12 +0900

    clean up code, commenting, spacing
    - needs AI

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
   var boardSpace = $(".board-space");
-  var playerMark = prompt("X or O").toUpperCase();
+  var playersMark = prompt("X or O").toUpperCase();
   var npcMark = function(){
-    if ( playerMark === "X" ) {
+    if ( playersMark === "X" ) {
       return "O";
     } else {
       return "X";
@@ -9,10 +9,7 @@
   };
   var allPossNpcMoves = [];
   var possNpcMoves = [];
-  var thisLinesOpenings = 0;
-  
-
-  var boardLines = { // these are possible winning lines
+  var winningLines = { 
     1 : ["1","2","3"], // 1,2,3
     2 : ["1","4","7"], // 1,4,7
     3 : ["1","5","9"], // 1,5,9
@@ -22,45 +19,39 @@
     7 : ["4","5","6"], // 4,5,6
     8 : ["7","8","9"]  // 7,8,9
   };
-  
-  var keys = Object.keys(boardLines);
+  var keys = Object.keys(winningLines);
   
   boardSpace.click(function(){
     var spaceIdRaw = this.id; // string "1"
-    var spaceId = spaceIdRaw.slice(1);
-    console.log(spaceId);
+    var spaceId = spaceIdRaw.slice(1); // remove "s" from ID 
     
-    if($(this).html() === "") { // if space empty
-      console.log("Where player clicked was empty"); 
-      $(this).html(playerMark); // put player's mark
-      for( var i = 1; i < keys.length; i++) { // cycle through keys
-        for ( var j = 0; j < 3; j++ ) { // cycle through values
-            thisLinesOpenings = 0;
-            if(boardLines[i][j] === ""){ // if just 1 open
-                thisLinesOpenings++;
-            }
-            
-            if ( spaceId === boardLines[i][j] ) { // get winning line 
-            console.log("this space was found in one of the lines");
-            allPossNpcMoves.push(...boardLines[i]);
-            possNpcMoves = allPossNpcMoves.filter(function(possibleMoves){
-              return possibleMoves !== spaceId;
-            });
-            console.log("NPC poss moves are " + possNpcMoves);
-                
-          } // end check for space in a winning row
-        } // end inner loop that searches a keys values to match the click
-      } // end outer loop to loop through keys
-    } // end check if space blank
+    if( $(this).html() === "" ) { 
+     
+        $(this).html(playersMark); 
+      
+        for( var i = 1; i < keys.length; i++ ) {
+        
+          for ( var j = 0 ; j < 3 ; j++ ) {
+              
+              if ( spaceId === winningLines[i][j] ) { // get winning line 
+                  allPossNpcMoves.push(...winningLines[i]);
+                  possNpcMoves = allPossNpcMoves.filter(function(possibleMoves){
+                    return possibleMoves !== spaceId;
+                  });
+                  
+              } 
+          } // end cycle through this key's values, wich are spaces in line 
+        } // end cycle through each board winning space key 
+    } // end check if clicked space is blank
     
-      for( var i = 0 ; i < possNpcMoves.length ; i++ ){ // loop through poss moves
-      if($("#s" + possNpcMoves[i]).html() === ""){ // if space is empty
-        $("#s" + possNpcMoves[i]).html(npcMark); // mark npcs mark
-        console.log("tried to mark npc in slot " + possNpcMoves[i]);
-        break; // was successfull, so loop can stop
-      }
-    }
-      // put AI here
+      for( var i = 0 ; i < possNpcMoves.length ; i++ ) { 
+          
+          if($( "#s" + possNpcMoves[i]).html() === "" ) { 
+              $( "#s" + possNpcMoves[i]).html(npcMark);
+              break; // was successfull, so loop can stop
+          } // end check if space is empty
+          
+      } // end loop through possible NPC moves ; add more logic here force win
   });
 
 
